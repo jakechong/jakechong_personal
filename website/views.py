8dc8e4d54ec94b5bd4c1c8c0bfb7669df4c7eb90
Jake Chong <jakechong@berkeley.edu>
2019-10-01 20:11:53 -0700

    view fix

--- website/views.py
+++ website/views.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+	pass
+
+def projects(request):
+	pass
+
+def experimental(request):
+	pass
