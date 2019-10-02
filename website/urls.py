94469a6c716e8ce155d36e6aeeca982035aa6053
Jake Chong <jakechong@berkeley.edu>
2019-10-01 22:53:18 -0700

    url resolver?

--- website/urls.py
+++ website/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from website import views as website_views
 
 website_urls = [
-    url(r'^/?$', website_views.home, name="home"),
-    url(r'^projects/?$', website_views.projects, name="projects"),
-    url(r'^experimental/?$', website_views.experimental, name="experimental")
+    url(r'^', website_views.home, name="home"),
+    url(r'^projects/', website_views.projects, name="projects"),
+    url(r'^experimental/', website_views.experimental, name="experimental")
 ]
