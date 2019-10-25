5e33bcd926cc25b21306f224e6a9ec59d71035e9
Jake Chong <jakechong@berkeley.edu>
2019-10-25 11:10:37 -0700

    commit this for now

--- jakechong_personal/settings.py
+++ jakechong_personal/settings.py
@@ -120,6 +120,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = 'assets'
+STATICFILES_DIRS = ()
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+)
 
 TEMPLATES = [
     {
